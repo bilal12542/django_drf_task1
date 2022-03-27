946571d829e744a5e50c66e304648125bbd00a5c
bilal12542 <bilal.12542@gmail.com>
2022-03-27 17:33:56 +0500

    registered all models in django

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Posts
+
+
+class AdminPosts(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'owner')
+
 
 # Register your models here.
+
+admin.site.register(Posts, AdminPosts)
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import MyUser
+
+
+class AdminUser(admin.ModelAdmin):
+    list_display = ('id', 'full_name', 'email', 'is_admin', 'is_active')
+    list_editable = ['is_active']
+
+    fieldsets = (
+        (None, {'fields': ('full_name', 'is_admin', 'is_active')}),
+    )
+    # add_fieldsets = (
+    #     (None, {
+    #         'fields': (
+    #             'password1', 'password2', 'full_name', 'is_admin', 'is_active')}
+    #      ),
+    # )
 
 # Register your models here.
+admin.site.register(MyUser, AdminUser)
